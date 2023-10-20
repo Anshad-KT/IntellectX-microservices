49da43a22e509241e109bfa1cfb9ecf62fd873de
Anshad-KT <anshadkt78@gmail.com>
2023-10-20 10:38:41 +0530

    production testing n6

--- tenants/index.ts
+++ tenants/index.ts
@@ -20,7 +20,7 @@ const start = async () => {
     await natsWrapper.connect( 
       "ticketing",
       //process.env.NATS_CLIENT_ID, 
-       "tenantww",       
+       "tenant",       
       "http://nats-srv:4222"
     )      
     
