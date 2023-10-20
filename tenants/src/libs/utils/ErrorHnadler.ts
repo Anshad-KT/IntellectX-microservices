56cca677cf18f8a6681953e66a52b3d226559c87
Anshad-KT <anshadkt78@gmail.com>
2023-10-20 11:50:29 +0530

    production testing n8

--- tenants/src/libs/utils/ErrorHnadler.ts
+++ tenants/src/libs/utils/ErrorHnadler.ts
@@ -1,17 +1,17 @@
-import { Request, Response, NextFunction } from "express";
-import { CommonResponse, ResponseError } from "./Response";
-export = (err: any, req: Request, res: Response, next: NextFunction) => {
-  console.log("why");
+// import { Request, Response, NextFunction } from "express";
+// import { CommonResponse, ResponseError } from "./Response";
+// export = (err: any, req: Request, res: Response, next: NextFunction) => {
+//   console.log("why");
   
-  const error = new ResponseError({
-    status: err.status || 500,
-    msg: (err.message || err.msg || "No message") as string,
-    reason: (err.reason || err.stack || "Somebody failed") as string,
-    url: req.originalUrl,
-    ip: req.ip,
-    validationErrors: [],
-  });
+//   const error = new ResponseError({
+//     status: err.status || 500,
+//     msg: (err.message || err.msg || "No message") as string,
+//     reason: (err.reason || err.stack || "Somebody failed") as string,
+//     url: req.originalUrl,
+//     ip: req.ip,
+//     validationErrors: [],
+//   });
 
-  res.status(error.status);
-  res.json(new CommonResponse({ status: false, error }));
-};
+//   res.status(error.status);
+//   res.json(new CommonResponse({ status: false, error }));
+// };
