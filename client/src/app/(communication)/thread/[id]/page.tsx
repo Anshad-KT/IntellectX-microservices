b66af43f4cc4f0064e9227d076da0c364ea59a75
Anshad-KT <anshadkt78@gmail.com>
2023-10-21 12:53:42 +0530

    production testing n10

--- client/src/app/(communication)/thread/[id]/page.tsx
+++ client/src/app/(communication)/thread/[id]/page.tsx
@@ -7,7 +7,7 @@ import useSWR from 'swr';
 import auth from '@/services/axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { addChannel } from '@/app/GlobalRedux/Features/channel/channelSlice';
-import { toast ,ToastContainer} from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 
 import { useRouter } from 'next/router';
 import { useParams } from 'next/navigation';
@@ -47,9 +47,9 @@ const Page = () => {
                         // Update the 'chat' property of the matching item
                         threadItem.chat = updatedChat;
                         console.log(threadItem);
-                        newMessage(threadItem?.chat?.from?.username,threadItem.chat?.content)
+                        newMessage(threadItem?.chat?.from?.username, threadItem.chat?.content)
                         console.log("done");
-                        
+
                         return threadItem;
                     } else {
                         return threadItem;
@@ -72,22 +72,22 @@ const Page = () => {
         dispatch(addEmployee(response.data));
         return response.data
     };
-    const newMessage = (user:string,content:string) =>{
+    const newMessage = (user: string, content: string) => {
         console.log("toast here");
-        
+
         toast.success(`${user}: ${content}`, {
-          position: "top-right",
-          autoClose: 3000,
-    });
+            position: "top-right",
+            autoClose: 3000,
+        });
     }
     const [isPopupActive, setIsPopupActive] = useState<boolean>(false);
 
     const togglePopup = () => {
-      setIsPopupActive(!isPopupActive); // Toggle the state from true to false or vice versa
+        setIsPopupActive(!isPopupActive); // Toggle the state from true to false or vice versa
     };
     const { value } = useSelector((state: RootState) => state.channel)
     const foundItem: any = value?.find((item: any) => item.id === id);
-   
+
     useEffect(() => {
         setDemos(foundItem)
     }, [])
@@ -142,17 +142,17 @@ const Page = () => {
     const { data, error } = useSWR(`/api/communication/getchannel`, fetchData);
     const { data: employeeData, error: employeeError } = useSWR('/api/company/getemployee', fetchEmployee);
     const [superUser, setSuperUser] = useState<any>({});
-   
-  useEffect(() => {
-    // Perform localStorage action
-    
-    const storedSuperUser = JSON.parse(localStorage.getItem("superUser") || "{}");
 
-    
-    setSuperUser(storedSuperUser)
-    
-  
-  }, []);
+    useEffect(() => {
+        // Perform localStorage action
+
+        const storedSuperUser = JSON.parse(localStorage.getItem("superUser") || "{}");
+
+        
+        setSuperUser(storedSuperUser)
+
+
+    }, []);
 
 
 
@@ -168,10 +168,14 @@ const Page = () => {
             <ToastContainer />
             <div className='grid grid-cols-12'>
                 {/* sidebar div */}
-                
-                     {superUser?.jwt ? <SideBar isOpen={isPopupActive} isSuperUser={true} /> : <SideBar isOpen={false} isSuperUser={false} />}
-           
-             
+
+                {superUser?.user ? (
+                    <SideBar isOpen={isPopupActive} isSuperUser={true} />
+                ) : (
+                    <SideBar isOpen={false} isSuperUser={false} />
+                )}
+
+
                 {/* content div */}
                 <div className='lg:w-full lg:h-screenc lg:pt-5  bg-secondary lg:col-span-10 col-span-12'>
                     <div className="h-1/6 bg-secondary flex justify-center items-end">
@@ -192,7 +196,7 @@ const Page = () => {
 
                             <div className='block pl-2 lg:flex h-full items-center justify-end mr-16 mb-auto bg-secondary  w-2/4 relative cursor-pointer'>
 
-                               {superUser?.user ? <PopupButton /> : ""}
+                                {superUser?.user ? <PopupButton /> : ""}
                                 <div className='bg-slate-400 ml-2 w-14 border-primary h-10 flex items-center justify-center rounded-md text-sm text-secondary hover:text-orange-500'>save</div>
 
 
@@ -229,7 +233,7 @@ const Page = () => {
                                 <div>
                                     <h6 className='text-sm'>Latest</h6>
                                     {/* threads */}
-                                  
+
                                     {demos?.threads
                                         ?.slice()
                                         .sort((a, b) => (b.chat.status ? 1 : -1))
@@ -237,9 +241,9 @@ const Page = () => {
                                             return (
                                                 <Link key={item.id} href={`/chat/${item.id}`}>
                                                     <div className='w-full my-2 h-24 hover:bg-primaryhover hover:text-white shadow bg-sidebar rounded-md mt-2 flex items-center pl-3 cursor-pointer'>
-                                                    <svg className='w-14 h-12 my-4' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-                                            </svg>
+                                                        <svg className='w-14 h-12 my-4' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                        </svg>
                                                         <div className='flex-col'>
                                                             <h4>{item.threadName}</h4>
                                                             <div className="flex">
@@ -257,7 +261,7 @@ const Page = () => {
                                                                 stroke="currentColor"
                                                                 className="w-6 h-6"
                                                             >
-                                                                
+
                                                             </svg>
                                                         )}
                                                         <div className="ml-auto mr-3 text-sm">
